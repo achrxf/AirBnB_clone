7e706b4aa9ea63daa47c555a0cf57a37f76564f4
achrxf <achrxf@hotmail.com>
2024-02-09 20:32:54 +0100

    review

---
+++ models/review.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Review."""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """Represent a review."""
+    place_id = ""
+    user_id = ""
+    text = ""
